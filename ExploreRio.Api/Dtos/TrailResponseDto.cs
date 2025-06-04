3e00cf6aa08caa7e08512f88d95b7d49c021c4b7
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-06-04 10:54:02 -0300

    feat: create DTOs for the Trail entity
    - add TrailResponseDto.cs for API responses
    - add CreateTrailDto.cs for POST requests
    - add UpdateTrailDto.cs for PUT requests

---
+++ ExploreRio.Api/Dtos/TrailResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ExploreRio.Api.Dtos
+{
+    public record class TrailResponseDto(
+        long Id,
+        string Name,
+        string Description,
+        string Keywords,
+        int Distance,
+        double Time,
+        string Difficult,
+        string Region
+        );
+}
---
+++ ExploreRio.Api/Dtos/UpdateTrailDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ExploreRio.Api.Dtos
+{
+    public record class UpdateTrailDto
+    (
+        string Name,
+        string Description,
+        string Keywords,
+        int Distance,
+        double Time,
+        string Difficult,
+        string Region
+    );
+}
+
