ee8bf56600e80145397556609cdec806dc29ac23
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-06-05 04:44:12 -0300

    refactor: fix indexing for Trail id.

--- ExploreRio.Api/Program.cs
+++ ExploreRio.Api/Program.cs
@@ -5,30 +5,25 @@ namespace ExploreRio.Api
 {
     public class Program
     {
+        private static int qtdTrilhas = 1;
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
-
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
-
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
             app.MapControllers();
 
+            //  Manual in memory data seeding
             List<TrailResponseDto> trailsList = new List<TrailResponseDto>();
             {
                 trailsList.Add(new TrailResponseDto(1, "Trilha Pedra da Gávea", "Trilha desafiadora com trechos de escalada leve, levando ao topo de uma das maiores formações rochosas à beira-mar do mundo, com vista panorâmica do Rio.", "montanha, mirante, floresta, aventura, zona sul", 6, 6.5, "dificil", "zona sul"));
@@ -36,10 +31,10 @@ namespace ExploreRio.Api
                 trailsList.Add(new TrailResponseDto(3, "Trilha Morro Dois Irmãos", "Trilha curta com vista incrível das praias do Leblon e Ipanema, acessada pela comunidade do Vidigal.", "mirante, favela tour, praia, floresta urbana", 2, 1.6, "facil", "zona sul"));
                 trailsList.Add(new TrailResponseDto(4, "Trilha da Pedra do Telégrafo", "Famosa por suas fotos em perspectiva, essa trilha oferece vista para praias selvagens e costões rochosos.", "mirante, foto, floresta, aventura, zona oeste", 4, 3.5, "media", "zona oeste"));
                 trailsList.Add(new TrailResponseDto(5, "Trilha Circuito das Praias Selvagens", "Percurso costeiro que conecta as praias do Meio, Funda, Inferno e Perigoso. Um dos mais belos circuitos do Rio.", "praia, floresta, costão, zona oeste, trilha longa", 8, 4.0, "media", "zona oeste"));
+                qtdTrilhas+=5;
             }
 
             const string GetTrailEndpointName = "getTrail";
-
             //GET /
             app.MapGet("/", () => "Explore Rio API");
             //GET /api/v1/trilhas
@@ -52,7 +47,7 @@ namespace ExploreRio.Api
             {
                 TrailResponseDto trail = new TrailResponseDto
                 (
-                    trailsList.Count + 1,
+                    qtdTrilhas,
                     newTrailDto.Name,
                     newTrailDto.Description,
                     newTrailDto.Keywords,
@@ -60,19 +55,16 @@ namespace ExploreRio.Api
                     newTrailDto.Time,
                     newTrailDto.Difficult,
                     newTrailDto.Region
-
                 );
                 trailsList.Add(trail);
-
+                qtdTrilhas += 1;
                 return Results.CreatedAtRoute(GetTrailEndpointName, new { id = trail.Id }, trail);
             });
             //PUT /api/v1/trilhas
             app.MapPut("/api/v1/trilhas/{id}", (int id, UpdateTrailDto updatedTrail) =>
             {
                 var index = trailsList.FindIndex(trail => trail.Id == id);
-
                 if (index == -1) return Results.NotFound();
-             
                 trailsList[index] = new TrailResponseDto(
                     id,
                     updatedTrail.Name,
@@ -83,24 +75,15 @@ namespace ExploreRio.Api
                     updatedTrail.Difficult,
                     updatedTrail.Region
                     );
-
                 return Results.NoContent();
-                
             });
-
             //DELETE /api/v1/trilhas
             app.MapDelete("/api/v1/trilhas/{id}", (int id) => {
-
                 int index = trailsList.FindIndex(trail =>  trail.Id == id);
-
                 if (index == -1) return Results.NotFound();
-
                 trailsList.RemoveAt(index);
-
                 return Results.NoContent();
-
             });
-
 
             app.Run();
         }
